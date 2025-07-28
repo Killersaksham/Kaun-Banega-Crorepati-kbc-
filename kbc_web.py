9b8401e6b6862cde69cd6efc1b1619a660d6d562
Saksham <sakshamsingh20aug2005@gmail.com>
2025-07-28 22:29:30 +0530

    Update kbc_web.py

--- kbc_web.py
+++ kbc_web.py
@@ -65,4 +65,6 @@ def get_question(index):
     return jsonify({"end": True})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
